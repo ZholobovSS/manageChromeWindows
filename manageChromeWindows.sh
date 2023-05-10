cf04ba4b71b4c202b75b415180f24d236782790d
ZholobovSS <zholobovsemyonsergeevich@gmail.com>
2023-05-10 20:08:34 +0300

    Github version

--- manageChromeWindows.sh
+++ manageChromeWindows.sh
@@ -4,10 +4,7 @@
 # VARIABLES
 
 TAB_1_LINK="URL for first tab"
-TAB_2_LINK="URL for second tab "
-TOKEN="This is additional variable. Depends on your specific task"
-HA_ENTITY_ID="This is additional variable. Depends on your specific task"
-
+TAB_2_LINK="URL for second tab"
 
 
 # IF DISPLAY VARIABLE NOT SET
@@ -21,24 +18,12 @@ fi
 
 CHROME_WINDOW_IDS=$(xdotool search --onlyvisible --name chrome)
 
-# GET INFO ABOUT MONITOR
-
-MONITOR_STATUS=$(curl --location "https://home.zh2s.ru/api/states/${HA_ENTITY_ID}" --header "Authorization: Bearer ${TOKEN}")
-
-
-echo $CHROME_WINDOW_IDS $MONITOR_STATUS
-
-# EXIT IF HA IS UNAVAILABLE
-
-if [[ -z $MONITOR_STATUS ]] ; then
-    exit 0;
-fi
 
 
 
-# OPEN CHROME WINDOWS IF MONITOR IS ON AND NO CHROME WINDOWS ARE OPEN
+# OPEN CHROME WINDOWS BY CONDITION
 
-if [[ $MONITOR_STATUS =~ "state\":\"on" && -z $CHROME_WINDOW_IDS ]] ; then
+if [[ set here some condition to open windows ]] ; then
     echo "Open chrome windows"
     google-chrome --new-window ${TAB_1_LINK} &
     sleep 5
@@ -56,9 +41,9 @@ fi
 
 
 
-# CLOSE CHROME IF MONITOR IS OFF
+# CLOSE ALL CHROME WINDOWS BY CONDITION
 
-if [[ $MONITOR_STATUS  =~ "state\":\"off" ]] ; then
+if [[ set here some condition to close all windows  ]] ; then
     echo "Close all chrome windows"
     killall chrome
 fi
